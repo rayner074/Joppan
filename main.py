63eb073a4ecc6a7168a7ee8dcea102cf6f888466
Thorappan7 <97897980+Thorappan7@users.noreply.github.com>
2022-01-19 16:04:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,11 @@ bot = Client (
   api_id = API_ID,
   api_hash = API_HASH,
   bot_token = BOT_TOKEN
+  plugins={
+                "root": "commands"
+            },
   )
   
-  
+  Print("alive")
+
 bot.run()
