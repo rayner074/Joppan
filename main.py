8069dbe315a04e5b946f119275984fdb88f0c1ef
Thorappan7 <97897980+Thorappan7@users.noreply.github.com>
2022-01-19 20:02:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pyrogram
 from pyrogram import Client, filters
 
 bot = Client(
-  "JanGo Bot"
+  "JanGo Bot",
   api_id = 11980417,
   api_hash = "0c04d148edcc0fb5ee0ef2254f7c1bc6",
   bot_token = "5040881620:AAF8bvIFscFDf2Dro9CHI78-U6T4S20ieQI",
