fe073279724edc1590f28818aaf5bb533d391799
Thorappan7 <97897980+Thorappan7@users.noreply.github.com>
2022-01-19 17:26:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,12 @@
 import pyrogram
 from pyrogram import Client, filters
 
-bot = Client (
+bot = Client 
   api_id = 11980417,
   api_hash = "0c04d148edcc0fb5ee0ef2254f7c1bc6",
   bot_token = "5040881620:AAF8bvIFscFDf2Dro9CHI78-U6T4S20ieQI",
   plugins={"root": "plugins"}
-  )
+  
   
 print("alive")
   
