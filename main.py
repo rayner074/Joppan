e425fc69f6a5fe2e6f7f9fac88d933c3c9581276
Thorappan7 <97897980+Thorappan7@users.noreply.github.com>
2022-01-19 20:56:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 
 import pyrogram
 from pyrogram import Client, filters
+from plugins import database
 
 bot = Client(
   "JanGo Bot",
   api_id = 11980417,
   api_hash = "0c04d148edcc0fb5ee0ef2254f7c1bc6",
   bot_token = "5040881620:AAF8bvIFscFDf2Dro9CHI78-U6T4S20ieQI",
-  plugins={"root": "plugins/database"}
   
   )
 print("alive")
