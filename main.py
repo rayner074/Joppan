0c4666889d99367e021a14565b367de55912ea7a
Thorappan7 <97897980+Thorappan7@users.noreply.github.com>
2022-01-19 17:22:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ bot = Client (
   api_id = 11980417,
   api_hash = "0c04d148edcc0fb5ee0ef2254f7c1bc6",
   bot_token = "5040881620:AAF8bvIFscFDf2Dro9CHI78-U6T4S20ieQI",
-  plugins={"root": "Commands"}
+  plugins={"root": "plugins"}
   )
   
 print("alive")
