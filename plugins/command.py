b572a344f66ad0a613791cd81d2f646e42ead380
Thorappan7 <97897980+Thorappan7@users.noreply.github.com>
2022-01-19 20:21:55 +0530

    Update command.py

--- plugins/command.py
+++ plugins/command.py
@@ -7,17 +7,18 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 JanGo = Client("JanGo Bot")
 
-@JanGo.on_message(filters.command(["start"]) & filters.private, group=1)
-async def start(bot, message):
-     bot.send_message(message.chat_id, "START_TEXT")
-        button = [[
+button = [[
            InlineKeyboardButton("help", callback_data="help"),
            InlineKeyboardButton("about", callback_data="about")
            ],[
            InlineKeyboardButton("close", callback_data="close")
            ]]
-       reply_markup = InlineKeyboardMarkup(button)
+      reply_markup = InlineKeyboardMarkup(button)
 
+
+@JanGo.on_message(filters.command(["start"]) & filters.private, group=1)
+async def start(bot, message):
+       bot.send_photo("me", "https://telegra.ph/file/6b6fe92e0a33b30e45303.jpg", caption="Holidays!")  
 
 
 START_TEXT = """
