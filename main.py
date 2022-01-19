91672a30158c4c404ddef99db4882a4adf04ceae
Thorappan7 <97897980+Thorappan7@users.noreply.github.com>
2022-01-19 16:40:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from . import Commands
+
 import pyrogram
 from pyrogram import Client, filters
 
@@ -6,7 +6,8 @@ bot = Client (
   "JanGo Bot",
   api_id = 11980417,
   api_hash = "0c04d148edcc0fb5ee0ef2254f7c1bc6",
-  bot_token = "5040881620:AAF8bvIFscFDf2Dro9CHI78-U6T4S20ieQI"
+  bot_token = "5040881620:AAF8bvIFscFDf2Dro9CHI78-U6T4S20ieQI",
+  plugins={"root": "Commands"}
   )
   
 print("alive")
