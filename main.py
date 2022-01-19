c725ef465d8ffeba19f8803497e2c27854276290
Thorappan7 <97897980+Thorappan7@users.noreply.github.com>
2022-01-19 15:55:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ from info import API_ID, API_HASH, BOT_TOKEN
 import pyrogram
 from pyrogram import Client
 
-JanGo = Client (
+bot = Client (
   "JanGo Bot",
   api_id = API_ID,
   api_hash = API_HASH,
   bot_token = BOT_TOKEN
   )
   
-  app = JanGo()
-  app.run()
+  
+  bot.run()
