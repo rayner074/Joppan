c7afad98d821fba489b5c02873c36d4315eddf23
Thorappan7 <97897980+Thorappan7@users.noreply.github.com>
2022-01-20 18:11:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,42 +1,13 @@
-
-import random
-import pyrogram 
-from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-
+from pyrogram import Client,filters
 
 bot = Client(
-  "JanGo Bot",
-  api_id = 11980417,
-  api_hash = "0c04d148edcc0fb5ee0ef2254f7c1bc6",
-  bot_token = "5065608218:AAFPSnj4CpXpqVe1kMKPtCpziCbtyB7FQZ8",
-  
-  )
-  
-  
-
-START_TEXT = "welcome brother"    
-   
-PICS = "https://telegra.ph/file/6b6fe92e0a33b30e45303.jpg"
-
-
-button = [
-         [ InlineKeyboardButton("help", callback_data="help"),
-           InlineKeyboardButton("about", callback_data="about")
-           ],[
-           InlineKeyboardButton("close", callback_data="close")]
-           ]
-      
-
-
-@bot.on_message(filters.command(["start"]) & filters.private)
+    "my first projrct",
+    api_id=502966,
+    api_hash="ba91c94ee88658b8702befa528544df3",
+    bot_token="5065608218:AAFPSnj4CpXpqVe1kMKPtCpziCbtyB7FQZ8"
+)
+@bot.on_message(filters.command("start") & filters.private)
 def start(bot, message):
-       text="START_TEXT"
-       reply_markup=InlineKeyboardMarkup(button)
-       message.replay(
-       text=text,
-       reply_markup=reply_markup,
-       disable_web_page_preview=True)
-       
-  
+    bot.send_message(message.chat.id,"hey bro")
+
 bot.run()
