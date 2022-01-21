9fcc83d6c87573dcd9706c51fc477ef5ec5f070f
Thorappan7 <97897980+Thorappan7@users.noreply.github.com>
2022-01-21 15:42:56 +0530

    Update and rename welcome.py to welcome_leave.py

---
+++ bot/welcome_leave.py
@@ -0,0 +1,19 @@
+from pyrogram import Client, filters, emoji
+
+
+MENTION = "[{}](tg://user?id={})"
+text1="hi{} {} welcome to Group Chat"
+
+
+group ="jangobotz"
+@bot.on_message(filters.chat(group) &filters.new_chat_members)
+async def welcome(bot, message):
+    new_members = [u.mention for u in message.new_chat_members]
+
+    TEXT2= text1.format(emoji.SPARKLES,",".join(new_members))
+    await message.reply_text(TEXT2)
+
+@bot.on_message(filters.command("leave") &filters.group)
+def leave(bot, message):
+    bot.send_message(message.chat.id, "നമ്മളില്ലേ...അല്ലേലും സ്വപ്നത്തിലെ കട്ടുറുമ്പ് ആവൻ ഞാനില്ല ..ഭൂമി ഉരുണ്ടതല്ലെ എവിടേലും വെച്ച് കാണാം")
+    bot.leave_chat(message.chat.id )
