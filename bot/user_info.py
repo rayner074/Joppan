ab7ef28bea51c44681406a2cd44fd73d5d3ca11c
Thorappan7 <97897980+Thorappan7@users.noreply.github.com>
2022-01-22 12:10:12 +0530

    Create user_info.py

---
+++ bot/user_info.py
@@ -0,0 +1,29 @@
+from pyrogram import Client as bot, filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
+import asyncio
+from info import Jk
+
+@bot.on_message(filters.command('id'))
+async def showid(client, message): 
+    chat_type = message.chat.type 
+    if chat_type == "private":
+        user_id = message.chat.id 
+        first = message.from_user.first_name 
+        last = message.from_user.last_name or "" 
+        username = message.from_user.username 
+        await message.reply_text(
+            f"<b>➲ First Name:</b> {first}\n<b>➲ Last Name:</b> {last}\n<b>➲ Username:</b> {username}\n<b>➲ Telegram ID:</b> <code>{user_id}</code>\n",
+            quote=True )
+
+
+@bot.on_message(filters.command('info'))
+async def showinfo(client, message): 
+    chat_type = message.chat.type 
+    if chat_type == "private":
+        user_id = message.chat.id 
+        first = message.from_user.first_name 
+        last = message.from_user.last_name or "None" 
+        username = message.from_user.username 
+        await message.reply_text(
+            f"<b>➲ First Name:</b> {first}\n<b>➲ Last Name:</b> {last}\n<b>➲ Username:</b> @{username}\n<b>➲ Telegram ID:</b> <code>{user_id}</code>\n <b>➲User Link:</b> <a href='tg://user?id={user_id}'><b>Click Here</b></a>\n",
+            quote=True )
