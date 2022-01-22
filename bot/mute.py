4611eaa9b5b0717d78091da3c9b83dcec42d7e12
Thorappan7 <97897980+Thorappan7@users.noreply.github.com>
2022-01-22 07:35:20 +0530

    Update mute.py

--- bot/mute.py
+++ bot/mute.py
@@ -14,9 +14,9 @@ def unban(bot, message):
 @bot.on_message(filters.command("mute")&filters.group)
 def mute(bot, message):
     bot.restrict_chat_member(message.chat.id, message.reply_to_message.from_user.id, ChatPermissions())
-    bot.send_message(message.chat.id, f"{message.reply_to_message.from_user.mention} please dont shout !!!")
+    bot.send_message(message.chat.id, f"{message.reply_to_message.from_user.mention} വിളച്ചിൽ എടുക്കരുത് കേട്ടോ .. കുറച്ച് നേരം മിണ്ടാതെ ഇരിക്ക് !!!")
 
 @bot.on_message(filters.command("unmute")&filters.group)
 def unmute(bot, message):
     bot.restrict_chat_member(message.chat.id, message.reply_to_message.from_user.id, ChatPermissions(can_send_media_messages=True, can_send_messages=True, can_send_other_messages=True))
-    bot.send_message(message.chat.id, f"{message.reply_to_message.from_user.mention} please dont shout again !!!")
+    bot.send_message(message.chat.id, f"{message.reply_to_message.from_user.mention} ഗോപാലകൃഷ്ണനോട് മൊതലാളി ഷമിച്ചിരിക്കുന്നു!!!")
