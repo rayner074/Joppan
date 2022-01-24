dc121d90684ef26587c88af68380e204678dbe64
Rayner 74 <96787752+rayner074@users.noreply.github.com>
2022-01-24 20:26:22 +0530

    Update botinfo.py

--- botinfo.py
+++ botinfo.py
@@ -1,1 +1,1 @@
-DATABASE_URI="mongodb+srv://poojabot: poojabot@cluster0.abcos.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+DATABASE_URI="mongodb+srv://poojabot:poojabot@cluster0.abcos.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
